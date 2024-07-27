fe74b0d8fd160266bd0c26f470f16c546c0f308c
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-07-27 13:20:34 +0530

    Update Best_Time_To_Buy_And_Sell_Stocks.java

--- Array/Best_Time_To_Buy_And_Sell_Stocks.java
+++ Array/Best_Time_To_Buy_And_Sell_Stocks.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max =0;
+        int current = prices[0];
+        for(int i = 1; i<prices.length; i++){
+            if(current < prices[i]){
+                if(max < ( prices[i]-current)) max = ( prices[i]-current);
+            }
+            else current =  prices[i];
+        }
+        return max;
 
+    }
+}
