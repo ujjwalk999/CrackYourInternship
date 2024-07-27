8ad5a997d001c126a1efe94632a7626e97a35ab5
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-07-27 13:18:25 +0530

    Update Move_Zeros.java

--- Array/Move_Zeros.java
+++ Array/Move_Zeros.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public void moveZeroes(int[] nums) {
+      if(nums.length == 1){
+        return ;
+      }
+      int i =0;
+      for(int j =0; j<nums.length; j++){
+        if(nums[j] !=0){
+            nums[i]= nums[j];
+            i++;
+        }
+      }
+        
+        while(i<nums.length){
+            nums [i] =0;
+            i++;
+        }
+    }
+}
