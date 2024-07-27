69a814a18ed845fdd735626fa9c88d1fcfb33cf5
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-07-27 13:14:31 +0530

    Update Chocolate_Distribution_Problem.java

--- Array/Chocolate_Distribution_Problem.java
+++ Array/Chocolate_Distribution_Problem.java
@@ -1,1 +1,14 @@
-
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        Collections.sort(a);
+        long maxi = Long.MAX_VALUE;
+        for(int i =0; i<= n-m; i++ ){
+            int min = a.get(i);
+            int max =a.get(i+m-1);
+            maxi = Math.min(maxi, (max-min));
+        }
+        return maxi;
+    }
+}
