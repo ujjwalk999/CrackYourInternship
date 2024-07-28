3f5b15b1d73ca51184bd6764b854ea1ca731cda7
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-07-28 22:52:24 +0530

    Update Sub_Array_Sum_Divisible_By_K.java

--- Array/Sub_Array_Sum_Divisible_By_K.java
+++ Array/Sub_Array_Sum_Divisible_By_K.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int count = 0, sum = 0, rem, size = nums.length;
+        int dp[] = new int[k];
 
+        for (int i = 0; i < size; i++) {
+            sum += nums[i];
+            rem = sum % k;
+
+            if (rem < 0) {
+                rem += k;
+            }
+
+            dp[rem]++;
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (dp[i] > 1) {
+                count += (dp[i] * (dp[i] - 1)) / 2;
+            }
+        }
+
+        count += dp[0];
+        return count;
+    }
+}
