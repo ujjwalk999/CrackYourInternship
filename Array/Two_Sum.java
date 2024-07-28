dbde3cc9711cb30f61263544e3b1bf8efb45e487
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-07-28 22:46:31 +0530

    Update Two_Sum.java

--- Array/Two_Sum.java
+++ Array/Two_Sum.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i = 0 ; i<nums.length ; i++ ){
+            int remain = target - nums[i];
+            if(mpp.containsKey(remain)  ){
+                return new int[]{mpp.get(remain),i};
+            }
+            mpp.put(nums[i],i);
+        }
+        return null;
+      
 
+        }
+    }
