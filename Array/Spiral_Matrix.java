e3663b73f129ba63187dc1b67b86470d772e99ad
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-07-30 23:45:48 +0530

    Update Spiral_Matrix.java

--- Array/Spiral_Matrix.java
+++ Array/Spiral_Matrix.java
@@ -1,1 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            // Move right across the top row
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Move down the right column
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Move left across the bottom row
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Move up the left column
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
