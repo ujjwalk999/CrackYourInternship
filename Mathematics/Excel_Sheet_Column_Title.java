0b62cd91d0bd8ec7dec25aa4a2dd40ccd6693601
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-08-06 16:45:52 +0530

    Update Excel_Sheet_Column_Title.java

--- Mathematics/Excel_Sheet_Column_Title.java
+++ Mathematics/Excel_Sheet_Column_Title.java
@@ -1,25 +1,12 @@
 class Solution {
-    public String convertToTitle(int cn) {
-        StringBuilder sb= new StringBuilder();
-        String alph="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if(cn<27){
-            return String.valueOf(alph.charAt(cn-1));
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            n--;
+            int remainder = n % 26;
+            sb.append((char) ('A' + remainder));
+            n /= 26;
         }
-        while(cn>26){
-            int temp= cn%26;
-            System.out.println(temp);
-            if(temp>0)
-            {
-                sb.append(alph.charAt(temp-1));
-                cn=cn/26;
-            }
-            else{
-                sb.append('Z');
-                cn /= 26;
-                cn -= 1;
-            }
-        }
-        sb.append(alph.charAt(cn-1));
-        return String.valueOf(sb.reverse());
+        return sb.reverse().toString();
     }
 }
