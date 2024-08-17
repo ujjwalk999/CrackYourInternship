851c4440957d27b143c4a562baa279176f207e4d
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-08-17 22:36:04 +0530

    Create Vertical Order Traversal of a Binary Tree.java

---
+++ Stack_Queues/Vertical Order Traversal of a Binary Tree.java
@@ -0,0 +1,46 @@
+class Solution {
+    class Pair{
+        int val;
+        int x;
+        int y;
+        Pair(int val,int x,int y){
+            this.val=val;
+            this.x=x;
+            this.y=y;
+        }
+    }
+    PriorityQueue<Pair> pq;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        pq=new PriorityQueue<>((a,b)->{
+            if(a.x!=b.x)
+            return a.x-b.x;
+            else
+            if(a.y!=b.y)
+            return a.y-b.y;
+            else
+            return a.val-b.val;
+        });
+        dfs(root,0,0);
+        // while(!pq.isEmpty()){
+        //     System.out.println(pq.peek().val+"\t"+pq.poll().x);
+        // }        
+        while(!pq.isEmpty()){
+            List<Integer> list=new ArrayList<>();
+            Pair poll=pq.poll();
+            list.add(poll.val);
+            while(!pq.isEmpty()&&pq.peek().x==poll.x){
+                list.add(pq.poll().val);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+    private void dfs(TreeNode root,int x,int y){
+        if(root==null)
+        return;
+        pq.offer(new Pair(root.val,x,y));
+        dfs(root.left,x-1,y+1);
+        dfs(root.right,x+1,y+1);
+    }
+}
