9917b2f24e4b741344421e70f607879bf65f1609
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-08-31 21:49:00 +0530

    Create Maximum Product Subarray.java

---
+++ Dynamic Programming/Maximum Product Subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int product = 1;
+        for(int i = 0 ;i< nums.length ; i++){
+            product = product * nums[i];
+            max = Math.max(max, product);
+            if(product == 0){
+                product =1;
+            }
+        }
+        product =1;
+        for(int i = nums.length-1 ;i >=0 ; i--){
+            product = product * nums[i];
+            max = Math.max(max, product);
+            if(product == 0){
+                product =1;
+            }
+        }
+        return max;
+    }
+}
