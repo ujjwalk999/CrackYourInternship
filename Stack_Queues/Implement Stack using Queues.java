04f78b74746b5b74d7da10c266b9eaa45b254501
ujjwalk999 <68048393+ujjwalk999@users.noreply.github.com>
2024-09-07 23:55:27 +0530

    Create Implement Stack using Queues.java

---
+++ Stack_Queues/Implement Stack using Queues.java
@@ -0,0 +1,26 @@
+class MyStack {
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.offer(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.offer(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
